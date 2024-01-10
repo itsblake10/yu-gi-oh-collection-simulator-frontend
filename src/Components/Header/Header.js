51fe78ccfb7b55b5746992e86a753be4bcecb0b3
itsblake10 <blake.cameron1001@gmail.com>
2024-01-11 01:57:42 +1300

    Add search-bar and sign-in/sign-up option / Fix horizontal scroll-bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,24 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
+        <div className="header__nav">
+          <div className="header__search-bar">
+            <form className="search-bar__form">
+              <input
+                className="search-bar__input"
+                type="text"
+                placeholder="Search..."
+              />
+              <button className="search-bar__button" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="header__profile">
+            <button className="header__sign-in">Sign In / Sign up</button>
+            {/*<p>PROFILE</p> */}
+          </div>
+        </div>
         <img
           className="header__logo"
           src={yugiohLogo}
